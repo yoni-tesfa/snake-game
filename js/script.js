45ac5298b5ab95d45831369ff351f43aec4af16b
meti yonas <yohanistasfa1996@gmail.com>
2025-07-29 15:59:03 +0300

    random x and y axis generating

--- js/script.js
+++ js/script.js
@@ -12,5 +12,11 @@ const columns = canvas.width / scale;
 // creating snake
 
 let snake = [];
-ctx.fillStyle = "white";
-ctx.fillRect(10, 10, scale, scale); // row ,column,20-width of rectangle,20-h of rectangle
+snake[0] = {
+  x: Math.floor(Math.random() * columns) * scale, // to generate random x axis
+  y: Math.floor(Math.random() * rows) * scale,
+};
+ctx.fillStyle = "white"; // snake color
+ctx.strokeStyle = "pink"; // pink border for snake
+ctx.fillRect(snake[0].x, snake[0].y, scale, scale); // row ,column,20-width of rectangle,20-h of rectangle
+ctx.strokeRect(snake[0].x, snake[0].y, scale, scale);
