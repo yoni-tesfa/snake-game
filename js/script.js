7304317cc3cdbe69533cd49c5d43f90a6b03398b
meti yonas <yohanistasfa1996@gmail.com>
2025-07-29 16:42:45 +0300

    moving the snake

--- js/script.js
+++ js/script.js
@@ -16,7 +16,39 @@ snake[0] = {
   x: Math.floor(Math.random() * columns) * scale, // to generate random x axis
   y: Math.floor(Math.random() * rows) * scale,
 };
-ctx.fillStyle = "white"; // snake color
-ctx.strokeStyle = "pink"; // pink border for snake
-ctx.fillRect(snake[0].x, snake[0].y, scale, scale); // row ,column,20-width of rectangle,20-h of rectangle
-ctx.strokeRect(snake[0].x, snake[0].y, scale, scale);
+
+// default snake direction
+let d = "right";
+
+// call draw function every 100 ms to draw the snake
+
+let playgame = setInterval(draw, 100);
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // removing every snake on canvas before creating new
+
+  for (i = 0; i < snake.length; i++) {
+    ctx.fillStyle = "white"; // snake color
+    ctx.strokeStyle = "red"; // red border for snake
+    ctx.fillRect(snake[i].x, snake[i].y, scale, scale); // row ,column,20-width of rectangle,20-h of rectangle
+    ctx.strokeRect(snake[i].x, snake[i].y, scale, scale);
+  }
+
+  //   old head position
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+  console.log(snakeX);
+
+  //   its moving  direction
+  if (d == "left") snakeX -= scale;
+  if (d == "up") snakeY -= scale;
+  if (d == "right") snakeY += scale;
+  if (d == "down") snakeY += scale;
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+  snake.pop();
+  snake.unshift(newHead);
+}
